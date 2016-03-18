186d2fb190959410ccced1cbe3a92baa1870ab87
Ioan Eugen Stan <eu@ieugen.ro>
2016-03-18 15:41:29 +0200

    Added handler for json api mediatype

--- katharsis-vertx/src/main/java/io/katharsis/vertx/JsonApiMediaType.java
+++ katharsis-vertx/src/main/java/io/katharsis/vertx/JsonApiMediaType.java
@@ -1,6 +1,11 @@
 package io.katharsis.vertx;
 
 //TODO: this should probably be in katharsis-core
+
+/**
+ * User {@link JsonApiMediaTypeHandler } version.
+ */
+@Deprecated
 public class JsonApiMediaType {
     /**
      * A {@code String} constant representing {@value #APPLICATION_JSON_API} media type.
